cb11baaf9acd47b7709d3c09a59a161c3a25bb40
Kavi <kavishkaprabashara@gmail.com>
2024-01-14 02:42:36 +0530

    Login Interface is completed.

--- src/main/java/lk/lakchat/chatapplicationinp/Controller/LoginForm.java
+++ src/main/java/lk/lakchat/chatapplicationinp/Controller/LoginForm.java
@@ -1,4 +1,33 @@
 package lk.lakchat.chatapplicationinp.Controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class LoginForm {
+
+    @FXML
+    private JFXButton btnAddPp;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private ImageView imgProPic;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnAddPpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
 }
